64b3c0c212382e3b7510af36b92ec82493b1b2d2
James Braddock Jr <jamesbraddockjr@Jamess-MacBook-Pro.local>
2021-01-03 22:59:29 -0600

    did not us js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-   $(".first-button").on("click", function () {
-      $(".animated-icon1").toggleClass("open");
-   });
-   $(".second-button").on("click", function () {
-      $(".animated-icon2").toggleClass("open");
-   });
-   $(".third-button").on("click", function () {
-      $(".animated-icon3").toggleClass("open");
-   });
+    $(".first-button").on("click", function () {
+        $(".animated-icon1").toggleClass("open");
+    });
+    $(".second-button").on("click", function () {
+        $(".animated-icon2").toggleClass("open");
+    });
+    $(".third-button").on("click", function () {
+        $(".animated-icon3").toggleClass("open");
+    });
 });
-function postYourAdd() {
-   var iframe = $("#forPostyouradd");
-   iframe.attr("src", iframe.data("src"));
-}
